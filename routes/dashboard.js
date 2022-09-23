01cda067ce17c5bcf4a7bbba4661fd55303465bc
Michael-A-I <zero.aduayi@gmail.com>
2022-09-23 01:02:46 -0400

    trouble shoot issue with dashboard

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -59,61 +59,71 @@ router.get("/dashboard/myTickets/:email", async (req, res) => {
 router.get("/dashboard/bugsandfeatures/:email", async (req, res) => {
   console.log("dashboard/bugsandfeatures")
 
-  const email = req.params.email
+  try {
+    const email = req.params.email
 
-  /* Speed increase due to not loading image and using lean */
-  const users = await Users.findOne({ email }).populate("myTickets")
+    /* Speed increase due to not loading image and using lean */
+    const users = await Users.findOne({ email }).populate("myTickets")
 
-  console.log("dashboard/bugsandfeatures")
+    console.log("dashboard/bugsandfeatures")
+    // ! on live server users is coming up as null
 
-  console.log(users)
-  const priority = [0, 0, 0, 0]
-  users.myTickets.forEach(user => {
-    if (user.priority == "low") {
-      console.log((priority[0] += 1))
-    }
-    if (user.priority == "medium") {
-      console.log((priority[1] += 1))
-    }
-    if (user.priority == "high") {
-      console.log((priority[2] += 1))
-    }
-    if (user.priority == "none") {
-      console.log((priority[3] += 1))
-    }
-  })
+    console.log({ users })
+    const priority = [0, 0, 0, 0]
 
-  console.log(priority)
-  return res.json(priority)
+    users.myTickets.forEach(user => {
+      if (user.priority == "low") {
+        console.log((priority[0] += 1))
+      }
+      if (user.priority == "medium") {
+        console.log((priority[1] += 1))
+      }
+      if (user.priority == "high") {
+        console.log((priority[2] += 1))
+      }
+      if (user.priority == "none") {
+        console.log((priority[3] += 1))
+      }
+    })
+
+    console.log(priority)
+    return res.json(priority)
+  } catch (error) {
+    console.log(error)
+  }
 })
 
 router.get("/dashboard/ticketstatus/:email", async (req, res) => {
   console.log("/dashboard/ticketstatus/")
 
-  const email = req.params.email
+  try {
+    const email = req.params.email
 
-  /* Speed increase due to not loading image and using lean */
-  const users = await Users.findOne({ email }).populate("myTickets")
+    /* Speed increase due to not loading image and using lean */
+    const users = await Users.findOne({ email }).populate("myTickets")
 
-  const status = [0, 0, 0, 0]
+    const status = [0, 0, 0, 0]
 
-  users.myTickets.forEach(user => {
-    if (user.status == "new") {
-      console.log((status[0] += 1))
-    }
-    if (user.status == "inprogress") {
-      console.log((status[1] += 1))
-    }
-    if (user.status == "completed") {
-      console.log((status[2] += 1))
-    }
-    if (user.status == "additional") {
-      console.log((status[3] += 1))
-    }
-  })
+    users.myTickets.forEach(user => {
+      if (user.status == "new") {
+        console.log((status[0] += 1))
+      }
+      if (user.status == "inprogress") {
+        console.log((status[1] += 1))
+      }
+      if (user.status == "completed") {
+        console.log((status[2] += 1))
+      }
+      if (user.status == "additional") {
+        console.log((status[3] += 1))
+      }
+    })
 
-  console.log(status)
-  return res.json(status)
+    console.log(status)
+    return res.json(status)
+  } catch (error) {
+    console.log(error)
+  }
 })
 
 module.exports = router
